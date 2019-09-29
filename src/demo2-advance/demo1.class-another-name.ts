615532f4b5f6b31aecb078cf2586d3838568e078
jinjian <jinj116@chinaunicom.cn>
2019-09-29 11:30:48 +0800

    9-29 commit

--- src/demo2-advance/demo1.class-another-name.ts
+++ src/demo2-advance/demo1.class-another-name.ts
@@ -12,3 +12,12 @@ function getName (n: nameOrNameResolver) :Name{
             return n();
         }
 }
+
+// 字符串字面量类型
+// 字符串字面量类型用来约束取值只能是某几个字符串中的一个
+
+type EventNames = 'click' | 'scroll' | 'mousemove';
+function handleEvent (ele: HTMLElement, event: EventNames) {
+
+}
+// handleEvent(document.getElementById('div'),'click')
